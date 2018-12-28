605a2333be395d2cc9e3a6a829bfafbb1c8eb7d4
ryongpintu <ryongpintu@gmail.com>
2018-12-28 18:12:23 +0530

    Updates

---
+++ precache-manifest.c56f85af92b60ca6df39249edcb9366a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/TD-React-Challenge-Test-1/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "0d868d1c74a756ad1658",
+    "url": "/TD-React-Challenge-Test-1/static/js/runtime~main.0d868d1c.js"
+  },
+  {
+    "revision": "5f29f5104548df12f080",
+    "url": "/TD-React-Challenge-Test-1/static/js/main.5f29f510.chunk.js"
+  },
+  {
+    "revision": "9795d1cf290fc024e25b",
+    "url": "/TD-React-Challenge-Test-1/static/js/1.9795d1cf.chunk.js"
+  },
+  {
+    "revision": "5f29f5104548df12f080",
+    "url": "/TD-React-Challenge-Test-1/static/css/main.b4f55096.chunk.css"
+  },
+  {
+    "revision": "30241250adec240ee484d1bf0b5a1202",
+    "url": "/TD-React-Challenge-Test-1/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.90721a7b507c88ae1353a537fbd15922.js"
+  "/TD-React-Challenge-Test-1/precache-manifest.c56f85af92b60ca6df39249edcb9366a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/TD-React-Challenge-Test-1/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
